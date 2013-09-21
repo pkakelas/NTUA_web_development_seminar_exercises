92f0d2b11e5a5b03f82c404a330475966c1b77a4
pkakelas <pkakelas@gmail.com>
2013-09-21 16:32:32 +0300

    Added validator to check if username exists

--- 6_exercise/controllers/user_validation_functions.php
+++ 6_exercise/controllers/user_validation_functions.php
@@ -1,17 +1,21 @@
 <?php
 	
 	function register_validation ($name, $surname, $age, $username, $password, $email) {
+		$exist = username_exists($username);
 		$problems = array();
+		if ($name == "" || $surname == "" || $age == "" || $password == "" || $username == "" || $email == "") {
+			$problems[] = "All forms are required";
+		}
 		if	(!is_numeric($age)) {
 			$problems[] = "Age must be a number";
 		}
-		if ($age<13) {
+		else if ($age<13) {
 			$problems[] = "You are to young to register to Boom Uploader..
-				Wait some years, until you are 13 and then discover the imagine world of  Boom Uploader !!";
+			Wait some years, until you are 13 and then discover the imagine world of  Boom Uploader !!";
 		} 
-		if ($name == "" || $surname == "" || $age == "" || $password == "" || $username == "" || $email == "") {
-			$problems[] = "All the forms are required in order to register.";
-		}
+		if ($exist) {
+			$problems[] = "The username exists. Please try another username.";
+		}	
 		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { 
 			$problems[] = "E-mail is not valid.";
 		}

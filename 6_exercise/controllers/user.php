88dd969d266be6cb4938f36ced201a759bda99b7
pkakelas <pkakelas@gmail.com>
2013-10-19 20:41:22 +0300

    Added Liscence, New resource called 'Session'

--- 6_exercise/controllers/user.php
+++ 6_exercise/controllers/user.php
@@ -45,28 +45,7 @@
 		public static function create_view() {
 			include 'views/user_create.php';
 		}
-        
-		public static function sign_in($username2, $password2) {
-			include 'models/sessions.php';
-			include 'models/user.php';
-			include 'models/sql.php';
-			$result = sign_in($username2, $password2);
-			if ($result) {
-		    	$_SESSION['username'] = $username;
-		    	header('Location: index.php?resource=file&method=create');
-			}
-			else {
-				include 'views/sign_in_false.php';	
-			}	
-		}
-
-		public static function sign_in_view() {
-			include 'views/sign_in.php';
-		}
-    
-        
-		
-	}
+   }
 
 ?>	
 	

559bc5576d6cf670af0366940074e1f18f4238d6
pkakelas <pkakelas@gmail.com>
2013-10-21 15:49:14 +0300

    revived uploader (still file_create controllers to go)

---
+++ 6_exercise/controllers/session.php
@@ -0,0 +1,24 @@
+<?php
+ 
+    class session_controller {
+
+		public static function create($username2, $password2) {
+		    include 'models/sessions.php';
+		    include 'models/user.php';
+		    include 'models/sql.php';
+		    $result = sign_in($username2, $password2);
+		    if ($result) {
+		        $_SESSION['username'] = $username;
+		        header('Location: index.php?resource=file&method=create');
+		    }
+		    else {
+		        include'views/session_create_false.php';  
+		    }   
+		}
+		
+		public static function create_view() {
+		    include 'views/session_create.php';
+		}
+    }
+
+?>	
